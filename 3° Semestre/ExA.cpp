2d68c6aa5a1a8c525f26deba0cc8e7435d2c01db
Gabriel Marcos Lopes Santos <147542361+GabrielMMMMM@users.noreply.github.com>
2024-05-21 20:04:20 -0300

    Add files via upload

---
+++ 3° Semestre/ExA.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+   int a;
+   cout << "Digite o valor de A: ";
+   cin >> a;
+   int b = a + 10;
+   system ("pause");
+   cout << b << endl;
+    return 0;
+}
