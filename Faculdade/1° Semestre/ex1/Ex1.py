d49322f0c0273e3ac17ecda035e99e015f57d3d8
Gabriel Marcos Lopes Santos <147542361+GabrielMMMMM@users.noreply.github.com>
2024-05-21 19:58:12 -0300

    Delete Faculdade/1° Semestre/ex1 directory

--- Faculdade/1° Semestre/ex1/Ex1.py
+++
@@ -1,8 +0,0 @@
-import time 
-nome = (input("Digite seu nome: "))
-senha = (input("Crie uma senha: "))
-while senha == nome :
-    senha =  (input("Crie uma senha que não seja seu nome: "))
-print ("Processando...")
-time.sleep(5)    
-print(f"Seu login : {nome} \nSua Senha: {senha}")
