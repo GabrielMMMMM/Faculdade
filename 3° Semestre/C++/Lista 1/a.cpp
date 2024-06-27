bfe83a19e16eb92e051f671fa7a2ec55cd7840ca
Gabriel Marcos Lopes Santos <147542361+GabrielMMMMM@users.noreply.github.com>
2024-06-27 15:57:31 -0300

    Add files via upload

---
+++ 3° Semestre/C++/Lista 1/a.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+#include "math.h"
+#include "cstdlib"
+using namespace std;
+int main() { 
+    system("cls");
+    setlocale(LC_ALL, "Portuguese");
+    double nota1=0, nota2=0, media=0;
+ cout << "\nDigite a nota 1";
+ cin >> nota1;
+ cout << "\nDigite as nota 2";
+cin >> nota2; 
+ media = (nota1 + nota2)/2;
+ cout << "\nO resultado ser� :" << media;
+cout << endl << endl;
+ system("pause");
+return 0; }
